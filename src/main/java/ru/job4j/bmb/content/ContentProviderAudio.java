e15c4941821f94d01ce7b59db2879e1f2fb2f1f2
Povvor <Povvor@yandex.ru>
2025-06-10 19:09:40 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

---
+++ src/main/java/ru/job4j/bmb/content/ContentProviderAudio.java
@@ -0,0 +1,17 @@
+package ru.job4j.bmb.content;
+
+import org.springframework.stereotype.Component;
+import java.io.File;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+
+
+@Component
+public class ContentProviderAudio implements ContentProvider {
+
+    @Override
+    public Content byMood(Long chatId, Long moodId) {
+        var content = new Content(chatId);
+        content.setAudio(new InputFile(new File("./audio/music.mp3")));
+        return content;
+    }
+}
--- src/main/java/ru/job4j/bmb/recomendations/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/recomendations/RecommendationEngine.java
@@ -2,8 +2,26 @@ package ru.job4j.bmb.recomendations;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import org.jvnet.hk2.annotations.Service;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.content.ContentProvider;
 
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class RecommendationEngine {
+    private final List<ContentProvider> contents;
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
+    }
+
+    public Content recommendFor(Long chatId, Long moodId) {
+        var index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
+    }
 
     @PostConstruct
     public void init() {
