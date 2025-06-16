9ef89f3efb2284beb8b9b0048bc67e48835e4473
Povvor <Povvor@yandex.ru>
2025-06-16 13:01:05 +0300

    Модели данных [#505302]

---
+++ src/main/java/ru/job4j/bmb/model/Achievement.java
@@ -0,0 +1,22 @@
+package ru.job4j.bmb.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "mb_achievement")
+public class Achievement {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private long createAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "award_id")
+    private Award award;
+    // getters, setters, hashCode, equals
+}
